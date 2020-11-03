b28117f0c2a43c8806a9b3a297422a10c8b419ea
KazumaShachou <raianimes1000@gmail.com>
2020-11-03 05:11:40 -0400

    moves

--- Assets/Scrips/Player.cs
+++ Assets/Scrips/Player.cs
@@ -8,6 +8,12 @@ public class Player : MonoBehaviour
     public Rigidbody2D rb;         //we had to create a var
 
     public float jumpforce;   
+
+    public LayerMask layerground;
+
+    private bool inground;
+
+    public float distancegroundminimum = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +30,12 @@ public class Player : MonoBehaviour
     
     }
     void jump() {
+        if (inground){
         rb.AddForce(Vector2.up * jumpforce);   //here we will add a force for jump, vector2 give us a type of force, y for up or down, and axis x is for right and left, jumpforce we will to create for to say how much force we will to use
+        }
     }
-
+    private void FixedUpdate() //functions that works with physics , we need to use fixedupdate, works each 0.2 sec
+     {   
+        inground = Physics2D.Raycast(transform.position, Vector2.down, 2f, layerground); //vecto2.down is a direction for down (-1 negative in axis y),distancegroundminimum is a distance, transform.positition is the exact position where dino is. 
+    }
 }
---
+++ Assets/Scrips/dinomove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dinomove : MonoBehaviour
+{
+
+    public Vector2 direction;
+
+    public float speed;
+
+      private  void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime); //vector 3 increase axis z, deltatime give to up the information about the time absolute between 1 frame and other (the time of each frame)
+    }
+}
