26ff22b93f90bc44004166013060b5d9b3723eda
KazumaShachou <raianimes1000@gmail.com>
2020-11-09 11:43:38 -0400

    cactus

---
+++ Assets/Scrips/Cactusgenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cactusgenerator : MonoBehaviour
+{
+    public GameObject cactusprefab;
+
+    public float delayinitial;
+
+    public float delaybetweencactus;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        InvokeRepeating("cactusgenerator", delayinitial, delaybetweencactus);
+
+
+    }
+
+    private void cactusgenerator()
+    {
+        Instantiate(cactusprefab, transform.position, Quaternion.identity);
+    }
+}
